5b0f1eb6ff722a0b1dfbd0633db4784788dc01a4
grinjk0418 <grinjk0418@gmail.com>
2025-11-20 18:10:07 +0900

    251120 login > service & repository, ORM(UserModel) set

---
+++ server/app/repositories/user.repository.js
@@ -0,0 +1,26 @@
+/**
+ * @file app/repositories/user.repository.js
+ * @description User Repository
+ * 251120 v1.0.0 jk init
+ */
+
+import db from '../models/index.js';
+const { User } = db;
+
+async function findByEmail(t = null, email) {
+  // SELECT * FROM users WHERE email = ? AND deleted_at IS NULL;
+  return await User.findOne( // User 적을때 자동완성 쓰면 모듈에 있는 다른거 나오니까 import 따로 적어야 됌
+    {
+      where: {
+        email: email
+      }
+    },
+    {
+      transaction: t
+    }
+  );
+}
+
+export default {
+  findByEmail,
+}
